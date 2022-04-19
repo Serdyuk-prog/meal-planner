2dc8d7ca534765dca17e6ee7391e2de065196934
Serdyuk-prog <tony.not.gloomy@gmail.com>
2022-04-19 19:37:03 +0700

    basic CRUD for simplified plan model

--- index.js
+++ index.js
@@ -30,7 +30,42 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
+app.get("/plans", async (req, res) => {
+    const plans = await Plan.find({});
+    res.render("plans/index", { plans });
+});
 
+app.get("/plans/new", (req, res) => {
+    res.render("plans/new");
+});
+
+app.post("/plans", async (req, res) => {
+    const plan = new Plan(req.body.plan);
+    await plan.save();
+    res.redirect(`/plans/${plan._id}`);
+});
+
+app.get("/plans/:id", async (req, res) => {
+    const plan = await Plan.findById(req.params.id);
+    res.render("plans/show", { plan });
+});
+
+app.get("/plans/:id/edit", async (req, res) => {
+    const plan = await Plan.findById(req.params.id);
+    res.render("plans/edit", { plan });
+});
+
+app.put("/plans/:id", async (req, res) => {
+    const { id } = req.params;
+    const plan = await Plan.findByIdAndUpdate(id, { ...req.body.plan });
+    res.redirect(`/plans/${plan._id}`);
+});
+
+app.delete('/plans/:id', async (req, res) => {
+    const { id } = req.params;
+    await Plan.findByIdAndDelete(id);
+    res.redirect('/plans');
+})
 
 app.listen(3000, () => {
     console.log("Listening оn port 3000");
